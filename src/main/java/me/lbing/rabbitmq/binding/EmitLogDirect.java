01b9bb7b217c40547b3c1cafa11d54e4055e4d5c
JeremyKing <jgroups@163.com>
2016-07-07 16:36:38 +0800

    Rabbit MQ bind

---
+++ src/main/java/me/lbing/rabbitmq/binding/EmitLogDirect.java
@@ -0,0 +1,51 @@
+package me.lbing.rabbitmq.binding;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class EmitLogDirect {
+
+	private static final String EXCHANGE_NAME = "direct_logs";
+
+	public static void main(String[] argv) throws java.io.IOException {
+
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+
+		channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+
+		String severity = getSeverity(argv);
+		String message = getMessage(argv);
+
+		channel.basicPublish(EXCHANGE_NAME, severity, null, message.getBytes());
+		System.out.println(" [x] Sent '" + severity + "':'" + message + "'");
+
+		channel.close();
+		connection.close();
+	}
+
+	private static String getSeverity(String[] argv) {
+		// TODO
+		return "info";
+	}
+
+	private static String getMessage(String[] strings) {
+		if (strings.length < 1)
+			return "Hello World!";
+		return joinStrings(strings, " ");
+	}
+
+	private static String joinStrings(String[] strings, String delimiter) {
+		int length = strings.length;
+		if (length == 0)
+			return "";
+		StringBuilder words = new StringBuilder(strings[0]);
+		for (int i = 1; i < length; i++) {
+			words.append(delimiter).append(strings[i]);
+		}
+		return words.toString();
+	}
+}
