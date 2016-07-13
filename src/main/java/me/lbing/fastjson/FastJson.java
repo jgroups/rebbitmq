34710d9c0203c2559013f20818ac61dfbf7697b6
JeremyKing <jgroups@163.com>
2016-07-13 17:04:16 +0800

    fastjson

---
+++ src/main/java/me/lbing/fastjson/FastJson.java
@@ -0,0 +1,15 @@
+package me.lbing.fastjson;
+
+public class FastJson {
+
+	public static <T> T fromJson(String json, T t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static String toJson(Object objectToConvert) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
